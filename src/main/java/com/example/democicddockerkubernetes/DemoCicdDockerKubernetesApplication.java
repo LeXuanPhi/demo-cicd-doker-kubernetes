05902035269b1854f68b604143d630354232bdd6
unknown <tridatinh213@gmail.com>
2022-07-19 22:02:30 +0700

    first commit

---
+++ src/main/java/com/example/democicddockerkubernetes/DemoCicdDockerKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.example.democicddockerkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example.controller")
+public class DemoCicdDockerKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCicdDockerKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/democicddockerkubernetes/DemoCicdDockerKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democicddockerkubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCicdDockerKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
