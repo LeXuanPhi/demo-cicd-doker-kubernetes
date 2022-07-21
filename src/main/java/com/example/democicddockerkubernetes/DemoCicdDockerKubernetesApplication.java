7151d07dbd3812a87e9752bb13ccebcc56f370b6
lexuanphi <tridatinh213@gmail.com>
2022-07-21 17:19:01 +0700

    phi.le push code

--- src/main/java/com/example/democicddockerkubernetes/DemoCicdDockerKubernetesApplication.java
+++ src/main/java/com/example/democicddockerkubernetes/DemoCicdDockerKubernetesApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoCicdDockerKubernetesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoCicdDockerKubernetesApplication.class, args);
-		System.out.println("");
 	}
 }
