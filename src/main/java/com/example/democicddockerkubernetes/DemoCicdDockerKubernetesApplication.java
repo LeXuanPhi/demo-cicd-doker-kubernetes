3c0761f2d73a15f8fef354146738ac38bf3cb5cf
lexuanphi <tridatinh213@gmail.com>
2022-07-21 18:13:21 +0700

    tao test

--- src/main/java/com/example/democicddockerkubernetes/DemoCicdDockerKubernetesApplication.java
+++ src/main/java/com/example/democicddockerkubernetes/DemoCicdDockerKubernetesApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoCicdDockerKubernetesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoCicdDockerKubernetesApplication.class, args);
-		System.out.println("");
+		System.out.println("oke");
 	}
 }
