83031290d5a453b01553214f8cd49119f2f10e77
lexuanphi <tridatinh213@gmail.com>
2022-07-23 19:22:58 +0700

    tao test 23

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -3,10 +3,14 @@ package com.example.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class DemoController {
+    Logger logger = Logger.getLogger(DemoController.class.getName());
     @RequestMapping(value = "/demo")
     public String Demo(){
+        logger.info("Tao test duoc roi");
         return "hello cicd-docker-kubernetes";
     }
 }
